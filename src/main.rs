6abd5c5ab0bdcb31701f90801d1d6c822858ed4e
CesarGBkR <yexotortuesprin@gmail.com>
2022-06-10 11:23:41 -0500

    Adding first challenge

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 fn main() {
+    //Obtener el nombre del usuario
     println!("Introduce tu nombre: ");
-    
-    // Obtener nombre por consola
     let mut nombre : String = String::new();
     std::io::stdin().read_line(&mut nombre).unwrap();
     nombre = nombre.trim().to_string();
-
-    // Obtener la edad por consola
-    println!("Introduce tu edad: ");
-    let mut edad : String = String::new();
-    std::io::stdin().read_line(&mut edad).unwrap();
     
-    // Comvertir esa edad en numero
+    //Obtener el edad del usuario
+    println!("Introduce tu país: ");
+    let mut pais : String = String::new();
+    std::io::stdin().read_line(&mut pais).unwrap();
+    pais = pais.trim().to_string();
 
-    let edad_int : u8 = edad.trim().parse().unwrap();
-
-    println!("Hola, bienvenidx {} de {} años", nombre, edad_int);
+    println!("Hola {} de {}", nombre, pais)
 }
