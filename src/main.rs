92b4864476b7621b67e25f5189b55c64865b1b98
CesarGBkR <yexotortuesprin@gmail.com>
2022-06-10 11:51:10 -0500

    Adding challeng 2

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-fn main() {
-    println!("Por favor introduce tu edad: ");
-    let mut edad: String = String::new();
-    std::io::stdin().read_line(&mut edad).unwrap();
+fn main(){
+    println!("¿Cuál es tu nombre?: ");
+    let mut nombre: String = String::new();
+    std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
+    
+    println!("Elige tu píldora, tienes dos opciones, la pildora azul(1) o la pildora roja(2): ");
+    let mut pildora: String = String::new();
+    std::io::stdin().read_line(&mut pildora).unwrap();
+    let pildora_int : u8 = pildora.trim().parse().unwrap();
 
-    let edad_int : u8 = edad.trim().parse().unwrap();
-
-    if edad_int >= 18 {
-        println!("Puedes entrar a la discoteca");
-    } if
-        edad_int >= 90 {
-            println!("pero tal vez no deberías entrar.");
-        } else {
-        println!("No puedes entrar a la discoteca, eres menor de edad");
+    if pildora_int == 1{
+        println!("Muy bien, {}. Sígueme, elegiste la pildora roja", nombre);
+    } else if pildora_int == 2{
+        println!("Como prefieras {}. No nos veremos nunca más, elegiste la pildora azul", nombre);
+    } else{
+        println!("Has elegido mal")
     }
-
-    println!("Tienes {} años", edad_int);
 }
