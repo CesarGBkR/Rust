4da189bc5a7641439efe3c5010ee3dfa569688e9
CesarGBkR <yexotortuesprin@gmail.com>
2022-06-10 10:58:21 -0500

    Printing code

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let temp_michoacan: i16 = 24;
+    let temp_cdmx:i16 = 19;
+    println!("La temperatura promedio en CDMX es {} mientras qeu en Michocacan es {}", temp_cdmx, temp_michoacan);
+}
