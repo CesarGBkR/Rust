d3ecbda08badded1be5bacf926afcd2efd7d196e
root <root@DESKTOP-JF6VGQS.localdomain>
2022-06-10 11:14:40 -0500

    Changing to reading function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
     println!("Introduce tu nombre: ");
     
+    // Obtener nombre por consola
     let mut nombre : String = String::new();
-
     std::io::stdin().read_line(&mut nombre).unwrap();
+    nombre = nombre.trim().to_string();
 
-    println!("Hola, bienvenidx {}", nombre);
+    // Obtener la edad por consola
+    println!("Introduce tu edad: ");
+    let mut edad : String = String::new();
+    std::io::stdin().read_line(&mut edad).unwrap();
+    
+    // Comvertir esa edad en numero
+
+    let edad_int : u8 = edad.trim().parse().unwrap();
+
+    println!("Hola, bienvenidx {} de {} años", nombre, edad_int);
 }
